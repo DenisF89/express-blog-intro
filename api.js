1a256f2713f78762914b8d6b23a2867d128a2b49
Denis89 <filippinidenis@gmail.com>
2026-02-25 21:07:54 +0100

    store con body request metodo post jsonparse

--- api.js
+++ api.js
@@ -4,8 +4,10 @@ const postsRouter = require('./routers/posts.js'); //recupera file di routing
 const app = express();					//creo server		
 const port = 3000;						//ingresso su dove il server riceve richieste
 
+app.use(express.json());                 //parsejson 
 app.use(express.static('public'));		//rendo disponibile al serve i file dentro cartella public
-app.use("/posts", postsRouter)          //contiene tutte le richieste fatte all'indirizzo /posts
+app.use("/posts", postsRouter);          //contiene tutte le richieste fatte all'indirizzo /posts
+
 
 app.get('/', (req, res) => {			//richiesta su rotta principale
 	res.send('<html><body><h1>Server del mio blog</h1></body></html>')
