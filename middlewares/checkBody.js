463de787e315d742fb533336181622c1a27418a0
Denis89 <filippinidenis@gmail.com>
2026-02-27 20:28:37 +0100

    debug title undefined con modify + requested anche per store

--- middlewares/checkBody.js
+++ middlewares/checkBody.js
@@ -3,16 +3,16 @@ function checkBody(req,res,next){
     let {title,tags} = req.body;
 
     //CONTROLLO STRINGA TITLE
-    
+    if(title !== undefined){
     if(String(title).length<3){                                         //Se il titolo non ha almeno 3 caratteri restituisci errore
         return res.status(400).json(                                    //torna errore json
                 { error: "Bad Request", 
                 message: "Il titolo è troppo corto", 
                 title}                                    
         );} 
-
+    }
     //CONTROLLO TAGS SEMPRE ARRAY
-    if(tags){
+    if(tags !== undefined){
     tags = (Array.isArray(tags)                                         //se array
                     ? tags                                              //lascia array
                     : String(tags).split(","))                          //se no trasforma in stringa, dividi con virgola e restituisci array
@@ -20,10 +20,12 @@ function checkBody(req,res,next){
                 const result = t.toLowerCase().trim();                  //normalizzo tags
                 return result.charAt(0).toUpperCase()+result.slice(1);  //inizio lettera maiuscola
                 }
-        );  
+        ); 
+    
+    req.body.tags = tags;                                               //aggiorno il body-request
     }
 
-    req.body = { ...req.body, title: String(title), tags };             //aggiorno il body-request
+    
     next();
 }
 
